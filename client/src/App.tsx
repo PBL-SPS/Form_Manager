297369e0f0e636d6eca4fc24e2c9a366f51f5395
Sourabh Kumbhar <kumsou6@gmail.com>
2021-11-23 11:43:29 +0530

    font changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import {
-  CircularProgress,
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
+    CircularProgress,
+    createTheme,
+    CssBaseline,
+    ThemeProvider,
 } from "@mui/material";
 import { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -17,61 +17,80 @@ import PrivateRoute from "./routes/PrivateRoute";
 import ProtectedRoutes from "./routes/ProtectedRoute";
 import PublicRoute from "./routes/PublicRoute";
 import { LS_KEYS } from "./utils/constants";
-import 'sweetalert2/dist/sweetalert2.min.css'
+import "sweetalert2/dist/sweetalert2.min.css";
 
 const queryClient = new QueryClient();
+let fontFamily = ['"Work Sans"'].join(",");
 const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
+    typography: {
+        fontFamily: fontFamily,
+        h5: {
+            fontFamily: fontFamily,
+        },
+    },
+    palette: {
+        mode: "dark",
+    },
 });
 function App() {
-  const authState = useAppSelector(selectAuthState);
-  const isAuthenticated =
-    authState === "LOGGEDIN" ||
-    localStorage.getItem(LS_KEYS.IS_LOGGED_IN) === "1";
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Router>
-            <Header />
-            <Suspense fallback={<CircularProgress color="primary" />}>
-              <Switch>
-                <PublicRoute path="/" exact isAuthenticated={isAuthenticated}>
-                  <Home />
-                </PublicRoute>
-                <PublicRoute
-                  path="/form/:formId"
-                  exact
-                  isAuthenticated={isAuthenticated}
-                >
-                  <Response />
-                </PublicRoute>
-                <PublicRoute path="/login" isAuthenticated={isAuthenticated}>
-                  <Login />
-                </PublicRoute>
+    const authState = useAppSelector(selectAuthState);
+    const isAuthenticated =
+        authState === "LOGGEDIN" ||
+        localStorage.getItem(LS_KEYS.IS_LOGGED_IN) === "1";
+    return (
+        <div
+            style={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={darkTheme}>
+                    <CssBaseline />
+                    <Router>
+                        <Header />
+                        <Suspense
+                            fallback={<CircularProgress color="primary" />}
+                        >
+                            <Switch>
+                                <PublicRoute
+                                    path="/"
+                                    exact
+                                    isAuthenticated={isAuthenticated}
+                                >
+                                    <Home />
+                                </PublicRoute>
+                                <PublicRoute
+                                    path="/form/:formId"
+                                    exact
+                                    isAuthenticated={isAuthenticated}
+                                >
+                                    <Response />
+                                </PublicRoute>
+                                <PublicRoute
+                                    path="/login"
+                                    isAuthenticated={isAuthenticated}
+                                >
+                                    <Login />
+                                </PublicRoute>
 
-                <PrivateRoute path="/" isAuthenticated={isAuthenticated}>
-                  <ProtectedRoutes />
-                </PrivateRoute>
-                <Route path="*">
-                  <div>Not found</div>
-                </Route>
-              </Switch>
-            </Suspense>
-          </Router>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </div>
-  );
+                                <PrivateRoute
+                                    path="/"
+                                    isAuthenticated={isAuthenticated}
+                                >
+                                    <ProtectedRoutes />
+                                </PrivateRoute>
+                                <Route path="*">
+                                    <div>Not found</div>
+                                </Route>
+                            </Switch>
+                        </Suspense>
+                    </Router>
+                </ThemeProvider>
+            </QueryClientProvider>
+        </div>
+    );
 }
 
 export default App;
